7c7f86d455f1cec09013bc1b6921665aadd5ef25
Teddy <lecomteteddy@gmail.com>
2020-03-06 11:30:45 +0100

    feature/front-Newproject

--- src/Form/ProjectType.php
+++ src/Form/ProjectType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Project;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,8 +16,12 @@ class ProjectType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('Description')
+            ->add('name', TextType::class, [
+                'label' => 'Nom du projet'
+            ])
+            ->add('Description', TextareaType::class, [
+                'label' => 'Description du projet'
+            ])
         ;
     }
 
