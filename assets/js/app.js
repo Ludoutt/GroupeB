ad6c639f0b223405be43c1a3bcc7d82d5d15f447
Julien Ramard <julien.ramard@gmail.com>
2020-03-06 11:43:07 +0100

    feat: imported icons library

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,12 @@
 import '../css/app.scss';
 
+// Bootstrap & jQuery
 import $ from 'jquery';
 import 'bootstrap';
+
+// Icons Library
+import feather from 'feather-icons';
+feather.replace();
 
 // $(document).ready(function () {
 //     console.log('Hello Webpack Encore');
