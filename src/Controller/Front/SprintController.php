5a53374f45ea8d5459c74775ae5573a11e1a01d2
Julien Ramard <julien.ramard@gmail.com>
2020-05-19 20:07:15 +0200

    refactor: replaced static data by tasks data on sprint views

--- src/Controller/Front/SprintController.php
+++ src/Controller/Front/SprintController.php
@@ -3,19 +3,31 @@
 namespace App\Controller\Front;
 
 use App\Repository\TaskRepository;
+use App\Repository\UserStoriesRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SprintController extends AbstractController
 {
     private $sprintTasks;
+    private $userStories;
+    private $users;
 
     /**
      * @param TaskRepository $taskRepository
+     * @param UserStoriesRepository $userStoriesRepository
+     * @param UserRepository $userRepository
      */
-    public function __construct(TaskRepository $taskRepository)
+    public function __construct(
+        TaskRepository $taskRepository,
+        UserStoriesRepository $userStoriesRepository,
+        UserRepository $userRepository
+    )
     {
         $this->sprintTasks = $taskRepository;
+        $this->userStories = $userStoriesRepository;
+        $this->users = $userRepository;
     }
 
     /**
@@ -25,6 +37,8 @@ class SprintController extends AbstractController
     {
         return $this->render('front/sprint/backlog/index.html.twig', [
             'sprintTasks' => $this->sprintTasks->findAll(),
+            'userStories' => $this->userStories->findAll(),
+            'users' => $this->users->findAll(),
         ]);
     }
 
@@ -35,6 +49,8 @@ class SprintController extends AbstractController
     {
         return $this->render('front/sprint/kanban/index.html.twig', [
             'sprintTasks' => $this->sprintTasks->findAll(),
+            'userStories' => $this->userStories->findAll(),
+            'users' => $this->users->findAll(),
         ]);
     }
 }
